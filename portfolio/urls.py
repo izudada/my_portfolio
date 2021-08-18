c7a7b85ad7625d3369cb8f014c17acc06a0aeea7
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-08-18 03:31:58 +0000

    initial commit

---
+++ portfolio/urls.py
@@ -0,0 +1,8 @@
+from django.urls import  include, path
+from . import views
+
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name="index"),
+    path('', include('django.contrib.auth.urls')),
+]
