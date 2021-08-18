d9c30badb254b5e8a4d22c11959c36fce64db757
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-08-18 21:37:28 +0000

    Add contact form view

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
-from django.views.generic import TemplateView
+from django.conf import settings
+from django.core.mail import send_mail
+
+from django.views.generic import TemplateView, FormView
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+def subscribe(request):
+    if request.method == 'POST':
+        sub = request.POST
+        subject = 'Welcome to DataFlair'
+        message = 'Hope you are enjoying your Django Tutorials'
+        recepient = str(sub['Email'].value())
+        send_mail(subject, 
+            message, settings.EMAIL_HOST_USER, [recepient], fail_silently = False)
+        return render(request, 'index.html', {'recepient': recepient})
+    return render(request, 'subscribe/index.html', {'form':sub})
