2268316e2e0f6ec91c86fa338a80951d44c50c8d
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-08-19 11:08:48 +0000

    Installed whitenoise

--- config/settings.py
+++ config/settings.py
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitwnoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
