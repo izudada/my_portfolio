0004ce74b4f712220b33ccf1bb21464c92415b68
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-08-20 07:37:50 +0000

    Remove internship section, change scroll reveal in contact, add zuri logo to footer, rewrite main.css

--- portfolio/static/js/main.js
+++ portfolio/static/js/main.js
@@ -71,10 +71,8 @@ sr.reveal('.skills__img', {delay: 400})
 sr.reveal('.portfolio__img', {interval: 200})
 
 /*SCROLL CONTACT*/
-// sr.reveal('.contact__subtitle', {})
-// sr.reveal('.contact__text', {interval: 200})
-// sr.reveal('.contact__input', {delay: 400})
-// sr.reveal('.contact__button', {delay: 600})
+sr.reveal('.contact__subtitle', {})
 
-
-
+sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__input', {delay: 400, interval: 0})
+sr.reveal('.contact__button', {delay: 600, interval: 0})
